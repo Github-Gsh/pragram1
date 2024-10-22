401c673024a9b7fe0771b99cfc0174a0a6607b3d
Github-Gsh <125450451+Github-Gsh@users.noreply.github.com>
2024-10-22 09:50:41 +0800

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test1
 @SpringBootApplication
 public class DemoApplication {
 
